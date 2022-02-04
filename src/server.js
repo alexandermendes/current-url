8b4f532efcf5bd0fbc8211c02caaafa2a255f842
Alex Mendes <alexander.mendes@immediate.co.uk>
2022-02-04 15:23:29 +0000

    fix: use more flexible request object validation

--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 import { IncomingMessage } from 'http';
 import originalUrl from 'original-url';
 
+const isIncomingMessage = (req) => (
+  req.name === 'IncomingMessage'
+  || req.constructor.name === 'IncomingMessage'
+  || req instanceof IncomingMessage
+);
+
 export const getCurrentUrl = (req, opts = {}) => {
   if (!req) {
     throw new Error('A request object is required to get the current URL on the server.');
   }
 
-  if (!(req instanceof IncomingMessage)) {
+  if (!isIncomingMessage(req)) {
     throw new Error('The request object must be an instance of `IncomingMessage`.');
   }
 
-  if (opts.original) {
-    return new URL(originalUrl(req).full);
+  if (opts.ignoreProxies) {
+    return new URL(req.url);
   }
 
-  return new URL(req.url);
+  return new URL(originalUrl(req).full);
 };
